665aef78b13e1329459bdb7f5a4e358c53a4d6dd
Juwon Yi <juunewon@gmail.com>
2023-08-09 17:57:36 +0900

    댓글 Entitiy 수정(08.09)

--- src/main/java/com/ohgiraffers/dailylogbackend/comment/command/domain/aggregate/entity/CommentEntity.java
+++ src/main/java/com/ohgiraffers/dailylogbackend/comment/command/domain/aggregate/entity/CommentEntity.java
@@ -1,13 +1,22 @@
 package com.ohgiraffers.dailylogbackend.comment.command.domain.aggregate.entity;
 
+import com.ohgiraffers.dailylogbackend.common.AuditingFields;
+import com.ohgiraffers.dailylogbackend.common.enumType.DeleteEnum;
 import com.ohgiraffers.dailylogbackend.diary.command.domain.aggregate.entity.DiaryEntity;
 import com.ohgiraffers.dailylogbackend.member.command.domain.aggregate.entity.MemberEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-
-public class Comment {
+@Entity
+@ToString
+@Setter
+@NoArgsConstructor
+public class CommentEntity extends AuditingFields {
 
     @Id
     @Column(name = "comment_no")
@@ -18,22 +27,20 @@ public class Comment {
     private String commentContent;
 
     @Column(name = "comment_is_deleted")
-    private boolean commentIsDeleted;
+    private DeleteEnum commentIsDeleted;
 
-    @Column(name = "comment_date")
-    private LocalDateTime commentDate;
-
+    @ManyToOne
     @JoinColumn(name = "diary_no")
     private DiaryEntity diaryNo;
 
+    @ManyToOne
     @JoinColumn(name = "member_no")
     private MemberEntity memberNo;
 
-    public Comment(Long commentNo, String commentContent, boolean commentIsDeleted, LocalDateTime commentDate, DiaryEntity diaryNo, MemberEntity memberNo) {
+    public CommentEntity(Long commentNo, String commentContent, DeleteEnum commentIsDeleted, DiaryEntity diaryNo, MemberEntity memberNo) {
         this.commentNo = commentNo;
         this.commentContent = commentContent;
         this.commentIsDeleted = commentIsDeleted;
-        this.commentDate = commentDate;
         this.diaryNo = diaryNo;
         this.memberNo = memberNo;
     }
