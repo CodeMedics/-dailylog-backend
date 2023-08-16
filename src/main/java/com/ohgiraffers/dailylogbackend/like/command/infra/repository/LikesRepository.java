00796caeaf575a57a8dac0b7dc184441f683ba97
dgw <ds257873@naver.com>
2023-08-17 07:48:58 +0900

    [Chore] 좋아요 리포지터리 일부 수정
    함수명 변경
    #44

--- src/main/java/com/ohgiraffers/dailylogbackend/like/command/infra/repository/LikesRepository.java
+++ src/main/java/com/ohgiraffers/dailylogbackend/like/command/infra/repository/LikesRepository.java
@@ -1,10 +1,12 @@
 package com.ohgiraffers.dailylogbackend.like.command.infra.repository;
 
+import com.ohgiraffers.dailylogbackend.diary.command.domain.aggregate.entity.DiaryEntity;
 import com.ohgiraffers.dailylogbackend.like.command.domain.aggregate.entity.LikesEntity;
+import com.ohgiraffers.dailylogbackend.member.command.domain.aggregate.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface LikesRepository extends JpaRepository<LikesEntity, Long> {
-    Optional<LikesEntity> findByMember_memberNoAndDiary_diaryNo(Long diaryNo, Long memberNo);
+    Optional<LikesEntity> findByMemberAndDiary(MemberEntity member, DiaryEntity diary);
 }
