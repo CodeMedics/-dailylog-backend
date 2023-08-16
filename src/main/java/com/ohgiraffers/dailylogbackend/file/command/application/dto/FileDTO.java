1c32625cc331d3a939fd7778002524ac08360784
minho jeong <wjdalsgh6951@gmail.com>
2023-08-16 19:04:37 +0900

    파일 디티오 구현

--- src/main/java/com/ohgiraffers/dailylogbackend/file/command/application/dto/FileDTO.java
+++ src/main/java/com/ohgiraffers/dailylogbackend/file/command/application/dto/FileDTO.java
@@ -1,4 +1,24 @@
 package com.ohgiraffers.dailylogbackend.file.command.application.dto;
 
+import com.ohgiraffers.dailylogbackend.common.enumType.DeleteEnum;
+import com.ohgiraffers.dailylogbackend.diary.command.domain.aggregate.entity.DiaryEntity;
+import com.ohgiraffers.dailylogbackend.member.command.domain.aggregate.entity.MemberEntity;
+import lombok.*;
+import org.hibernate.sql.Delete;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
 public class FileDTO {
+
+    private Long fileNo;
+    private String originFileName;
+    private String fileName;
+    private String filePath;
+    private MemberEntity member;
+    private DiaryEntity diaryNo;
+    private DeleteEnum isDelete;
+
 }
