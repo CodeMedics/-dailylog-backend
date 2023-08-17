7712440139d4fdf04650d8529d7083557f4a2e12
ej20001000 <ejayyj1225@gmail.com>
2023-08-17 15:57:13 +0900

    [Feat] 일기 테스트 수정 dataJpaTest configuration add #43

--- src/test/java/com/ohgiraffers/dailylogbackend/diary/DiaryWriteTest.java
+++ src/test/java/com/ohgiraffers/dailylogbackend/diary/DiaryWriteTest.java
@@ -1,15 +1,11 @@
 package com.ohgiraffers.dailylogbackend.diary;
 
-import com.ohgiraffers.dailylogbackend.common.enumType.DeleteEnum;
 import com.ohgiraffers.dailylogbackend.diary.command.application.dto.DiaryWriteDTO;
 import com.ohgiraffers.dailylogbackend.diary.command.application.service.DiaryServiceImpl;
 import com.ohgiraffers.dailylogbackend.diary.command.domain.aggregate.entity.DiaryEntity;
 import com.ohgiraffers.dailylogbackend.diary.command.infra.repository.DiaryRepository;
-import com.ohgiraffers.dailylogbackend.member.command.domain.aggregate.entity.MemberEntity;
-import org.junit.jupiter.api.*;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -17,8 +13,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 //@DataJpaTest // 데이터 롤백됨
@@ -27,6 +21,9 @@ public class DiaryWriteTest {
 
     @Autowired
     private DiaryServiceImpl diaryServiceImpl;
+
+//    @Autowired
+//    private DiaryRepository diaryRepository;
 
     @Test
     @DisplayName("diary insert test")
@@ -37,8 +34,15 @@ public class DiaryWriteTest {
                 .feelCategory("good")
                 .build();
 
+//        DiaryEntity de = DiaryEntity.builder()
+//                .diaryContent("lol")
+//                .feelCategory("good")
+//                .build();
+
         // Act
         DiaryEntity diaryEntity = diaryServiceImpl.writeDiary(diaryWriteDTO);
+
+//        DiaryEntity diaryEntity = diaryRepository.save(de);
 
         // Assert
         assertNotNull(diaryEntity);
