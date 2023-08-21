120dc5015077535b5a58e287857662fd659d5ef6
ej20001000 <ejayyj1225@gmail.com>
2023-08-21 19:45:14 +0900

    [Fix] Test 코드 통합 #43

---
+++ src/test/java/com/ohgiraffers/dailylogbackend/diary/DiaryTest.java
@@ -0,0 +1,81 @@
+package com.ohgiraffers.dailylogbackend.diary;
+
+import com.ohgiraffers.dailylogbackend.common.enumType.DeleteEnum;
+import com.ohgiraffers.dailylogbackend.diary.command.application.dto.DiaryUpdateDTO;
+import com.ohgiraffers.dailylogbackend.diary.command.application.dto.DiaryWriteDTO;
+import com.ohgiraffers.dailylogbackend.diary.command.application.service.DiaryService;
+import com.ohgiraffers.dailylogbackend.diary.command.domain.aggregate.entity.DiaryEntity;
+import com.ohgiraffers.dailylogbackend.member.command.domain.aggregate.entity.MemberEntity;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class DiaryTest {
+
+    @Autowired
+    private DiaryService diaryService;
+
+    @Autowired
+    private InsertFakeUser insertFakeUser;
+
+    public MemberEntity setUp() {
+        return insertFakeUser.insertFakeUser();
+    }
+
+
+    @Test
+    @DisplayName("diary insert test")
+    public void testWriteDiary() {
+        // Arrange
+
+        MemberEntity fakeUser = setUp();
+
+        MemberEntity memberEntity = MemberEntity.builder()
+                .memberNo(fakeUser.getMemberNo())
+                .build();
+
+        DiaryWriteDTO diaryWriteDTO = new DiaryWriteDTO(memberEntity, "create", "create");
+
+        // Act
+        DiaryEntity diaryEntity = diaryService.writeDiary(diaryWriteDTO);
+
+        // Assert
+        assertNotNull(diaryEntity);
+        assertEquals(diaryWriteDTO.getDiaryContent(), diaryEntity.getDiaryContent());
+        assertEquals(diaryWriteDTO.getFeelCategory(), diaryEntity.getFeelCategory());
+
+    }
+
+    @Test
+    @DisplayName("diary update test")
+    public void diaryUpdateTest() {
+        // Arrange
+
+        DiaryUpdateDTO diaryUpdateDTO = new DiaryUpdateDTO("update", "update");
+
+        // Act
+        DiaryEntity updatedDiary = diaryService.updateDiary(1L, diaryUpdateDTO);
+
+        // Assert
+        assertNotNull(updatedDiary);
+        assertEquals(diaryUpdateDTO.getDiaryContent(), updatedDiary.getDiaryContent());
+        assertEquals(diaryUpdateDTO.getFeelCategory(), updatedDiary.getFeelCategory());
+    }
+
+    @Test
+    @DisplayName("diary delete test")
+    public void diaryDeleteTest() {
+        // Arrange
+
+        // Act
+        DiaryEntity deletedDiary = diaryService.deleteDiary(1L);
+
+        // Assert
+        assertEquals(deletedDiary.getIfDelete(), DeleteEnum.DELETED);
+
+    }
+}
--- src/test/java/com/ohgiraffers/dailylogbackend/diary/InsertFakeUser.java
+++ src/test/java/com/ohgiraffers/dailylogbackend/diary/InsertFakeUser.java
@@ -1,36 +1,36 @@
-//package com.ohgiraffers.dailylogbackend.diary;
-//
-//import com.ohgiraffers.dailylogbackend.member.command.domain.aggregate.EnumType.SocialEnum;
-//import com.ohgiraffers.dailylogbackend.member.command.domain.aggregate.entity.MemberEntity;
-//import com.ohgiraffers.dailylogbackend.member.command.domain.repository.MemberRepository;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//
-//@SpringBootTest
-//public class InsertFakeUser {
-//
-//    @Autowired
-//    private MemberRepository memberRepository;
-//
-//
-//    @Test
-//    public void insertFakeUser() {
-//
-//        for (int i = 0; i < 10; i++) {
-//            MemberEntity memberEntity = MemberEntity.builder()
-//                    .UID("fake")
-//                    .nickname("fake" +  i)
-//                    .social(SocialEnum.KAKAO)
-//                    .email("fake@email.com")
-//                    .accessToken("fake")
-//                    .accessTokenExpireDate(1L)
-//                    .refreshToken("fake")
-//                    .refreshTokenExpireDate(1L)
-//                    .build();
-//
-//            memberRepository.save(memberEntity);
-//        }
-//    }
-//}
+package com.ohgiraffers.dailylogbackend.diary;
+
+import com.ohgiraffers.dailylogbackend.diary.command.domain.aggregate.entity.DiaryEntity;
+import com.ohgiraffers.dailylogbackend.member.command.domain.aggregate.EnumType.SocialEnum;
+import com.ohgiraffers.dailylogbackend.member.command.domain.aggregate.entity.MemberEntity;
+import com.ohgiraffers.dailylogbackend.member.command.domain.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class InsertFakeUser {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+
+    public MemberEntity insertFakeUser() {
+        MemberEntity memberEntity = MemberEntity.builder()
+                .UID("fake")
+                .nickname("fake")
+                .social(SocialEnum.KAKAO)
+                .email("fake@email.com")
+                .accessToken("fake")
+                .accessTokenExpireDate(1L)
+                .refreshToken("fake")
+                .refreshTokenExpireDate(1L)
+                .build();
+
+        return memberRepository.save(memberEntity);
+    }
+
+    public void deleteFakeUser(MemberEntity memberEntity) {
+        memberRepository.deleteById(memberEntity.getMemberNo());
+    }
+}
